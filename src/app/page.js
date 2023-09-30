1165948591766c846bb1ae9e5e8de893d3851ddb
NVB07 <nguyenvanbinh20020712@gmail.com>
2023-09-30 16:23:31 +0700

    rm class

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
     const query = "/api/proxy?type=artist,song,key,code&num=500&query={query}";
 
     return (
-        <main className={styles.main}>
+        <main>
             <code>api strat on: MyHost{query}`</code>
         </main>
     );
